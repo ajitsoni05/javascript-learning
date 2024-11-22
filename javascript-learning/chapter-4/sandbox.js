13788d55534e92e6a8fbad3fa58a7dc3cc1387d6
Ajit Soni <ajitsoni000005@gmail.com>
2024-11-22 17:43:12 +0530

    add: function declaration, invokation and expression

---
+++ javascript-learning/chapter-4/sandbox.js
@@ -0,0 +1,19 @@
+// functions
+function greet() {
+  console.log("Hello there!");
+}
+
+// function invokation
+greet();
+greet();
+greet();
+
+// function expression
+const speak = function () {
+  console.log("Goodday!");
+
+};
+
+speak();
+speak();
+speak();
