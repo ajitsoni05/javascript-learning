fa1f5ebb576902eb8942ca42c5b0867121ebd60e
Ajit Soni <ajitsoni000005@gmail.com>
2024-11-22 17:51:07 +0530

    add: arguments and parameters

--- javascript-learning/chapter-4/sandbox.js
+++ javascript-learning/chapter-4/sandbox.js
@@ -11,12 +11,20 @@ greet();
 // function expression
 const speak = function () {
   console.log("Goodday!");
-
 };
 
 speak();
 speak();
 speak();
 
-
 // NOTE: function declarations are  hoisted while the function expression isn't hoisted
+
+// arguments and parameters
+
+// here name is parameter 
+const greeting = function (name) {
+  console.log(`Hello, ${name}!`);
+};
+
+// argument
+greeting("John");
