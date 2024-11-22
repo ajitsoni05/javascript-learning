202398d59c60050d86cb9daf01ee9922db51d15d
Ajit Soni <ajitsoni000005@gmail.com>
2024-11-22 17:12:50 +0530

    add: use of logical OR

--- javascript-learning/chapter-3/sandbox.js
+++ javascript-learning/chapter-3/sandbox.js
@@ -48,12 +48,15 @@ if (ninjas.length > 3) {
   console.log("that's a lot of ninjas");
 }
 
-const password = "p@sswordings";
+const password = "p@ssw";
 
 if (password.length >= 12 && password.includes("@")) {
   console.log("that password is mighty strong");
-} else if (password.length >= 8) {
-  console.log("that password is long enough");
+} else if (
+  password.length >= 8 ||
+  (password.includes("@") && password.length >= 5)
+) {
+  console.log("that password is strong enough");
 } else {
-  console.log("password is not long enough");
+  console.log("password is not strong enough");
 }
