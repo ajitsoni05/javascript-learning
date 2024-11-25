a880e6e47a12049fe1761983a621121f92f6ca65
Ajit Soni <ajitsoni000005@gmail.com>
2024-11-25 11:27:51 +0530

    add : object literals

---
+++ javascript-learning/chapter-5/sandbox.js
@@ -0,0 +1,50 @@
+let user = {
+  name: "crystal",
+  age: 30,
+  email: "crystal@netninja.co.uk",
+  location: "berlin",
+  blogs: ["why mac and cheese", "10 things to make with marmite"],
+};
+
+console.log(user);
+
+// accessing properties with dot notation
+console.log(user.name);
+
+// accessing properties with square brackets notation
+console.log(user["email"]);
+
+// updating properties with square brackets notation
+user["location"] = "london";
+console.log(user.location);
+
+// change properties
+user.age = 31;
+console.log(user.age);
+
+// using square bracket notation with a variable
+const key = "location";
+
+console.log(user[key]);
+
+// typeof
+console.log(typeof user); // object
+
+console.log(typeof user["name"]); // string
+
+console.log(typeof user.location); // string
+
+user = {
+  name: "crystal",
+  age: 30,
+  email: "crystal@netninja.co.uk",
+  location: "berlin",
+  blogs: ["why mac and cheese", "10 things to make with marmite"],
+  // methods are function defined inside the object
+  login: function () {
+    console.log("user is logged in");
+  },
+};
+
+user.login();
+
