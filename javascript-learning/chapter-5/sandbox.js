b0c2f7d8c881c9da1323bc82a3c28116504a49a6
Ajit Soni <ajitsoni000005@gmail.com>
2024-11-25 12:11:01 +0530

    add: Math object

--- javascript-learning/chapter-5/sandbox.js
+++ javascript-learning/chapter-5/sandbox.js
@@ -86,3 +86,21 @@ user = {
 };
 
 user.logLocation();
+
+// Math object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.7;
+
+console.log(Math.round(area));
+console.log(Math.floor(area));
+console.log(Math.ceil(area));
+console.log(Math.trunc(area));
+
+const random = Math.random();
+
+// generates a random number between 0 and 100
+console.log(Math.round(random * 100));
